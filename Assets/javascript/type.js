6a3266f11ac856404844a052ac98add3438bc7ac
Rob <rjr211@scarletmail.rutgers.edu>
2018-09-10 18:55:55 -0400

    revised

---
+++ Assets/javascript/type.js
@@ -0,0 +1,27 @@
+// logic here
+function typeText(element, speed){
+  //   save text to write
+  var textToWrite = element.textContent;
+  
+  //   Remove text from element
+  element.innerHTML = "";
+  
+  //   size of text for cicle
+  var textSize = textToWrite.length;
+  
+  //   Setting up the text on screen
+  for (var i=0; i<textSize; i++){    
+      (function(i) {
+          setTimeout(function(){
+            element.innerHTML+=textToWrite.charAt(i);
+          }, speed * i);
+      })(i);
+  }
+}
+
+
+var elementToWrite = document.getElementById("text");
+
+var speedOfEffect = 150;
+
+this.typeText(elementToWrite, speedOfEffect);
