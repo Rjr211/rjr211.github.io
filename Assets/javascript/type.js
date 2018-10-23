65e5e70a70c3daff78f48934579d228a752cab18
Rob <rjr211@scarletmail.rutgers.edu>
2018-10-22 23:18:11 -0400

    re-design

--- Assets/javascript/type.js
+++ Assets/javascript/type.js
@@ -1,27 +1,3 @@
-// logic here
-function typeText(element, speed){
-  //   save text to write
-  var textToWrite = element.textContent;
-  
-  //   Remove text from element
-  element.innerHTML = "";
-  
-  //   size of text for cicle
-  var textSize = textToWrite.length;
-  
-  //   Setting up the text on screen
-  for (var i=0; i<textSize; i++){    
-      (function(i) {
-          setTimeout(function(){
-            element.innerHTML+=textToWrite.charAt(i);
-          }, speed * i);
-      })(i);
-  }
-}
-
-
-var elementToWrite = document.getElementById("text");
-
-var speedOfEffect = 200;
-
-this.typeText(elementToWrite, speedOfEffect);
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
