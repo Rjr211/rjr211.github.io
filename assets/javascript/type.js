f3cf93ae4239b8b4e0d94f400762c4fc7c047d7e
Rob <rjr211@scarletmail.rutgers.edu>
2018-11-01 21:09:04 -0400

    scoll to top btn

--- assets/javascript/type.js
+++ assets/javascript/type.js
@@ -1,4 +1,23 @@
+//Parallax functionality
 $(document).ready(function(){
   $('.parallax').parallax();
 });
 
+$(document).ready(function () {
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollUp').fadeIn();
+    } else {
+      $('.scrollUp').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollUp').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+});
